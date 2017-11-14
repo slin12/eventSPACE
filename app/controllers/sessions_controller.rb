0b4953ba1ba846f04ed42f6c96e163e8b16eb8f3
Abe Rudy <aberudy@gmail.com>
2017-11-14 09:33:35 -0500

    fixed merge issues, take 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,5 @@
 class SessionsController < ApplicationController
 
-<<<<<<< HEAD
-  def create
-   @user = User.find_by(email: params[:user][:email])
-     if @user.authenticate(params[:user][:password])
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-    else
-      flash.now[:notice] = "Login credentials were incorrect. Check your email and password and try again."
-      @user = User.new
-      render '/'
-    end
-  end
-=======
 def create
   @user = User.find_by(email: params[:user][:email])
   if @user.authenticate(params[:user][:password])
@@ -25,7 +12,6 @@ def create
     render 'static/home'
   end
 end
->>>>>>> ux_dashboard
 
   def destroy
     session[:user_id] = nil
