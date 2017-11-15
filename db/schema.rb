7e77eda3bf2bd36243f8636ddbbf543edbfc75c3
slin12 <s.lin12@gmail.com>
2017-11-15 16:22:29 -0500

    remove images

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171115175833) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "title"
