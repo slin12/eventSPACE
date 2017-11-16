d007bbb0a90cf3789f1c73fef6e15ebc2890129e
Abe Rudy <aberudy@gmail.com>
2017-11-16 10:46:02 -0500

    profile uploader fix

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -10,7 +10,7 @@ class ProfileUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
+  def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
 
